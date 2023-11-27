3ebb8508defe9a2057a0a68dd96d44616f8fe85a
Hunter Krasa <hunterkrasa1@gmail.com>
2023-11-27 14:34:55 -0800

    elastic works

--- crawl.py
+++ crawl.py
@@ -1,28 +1,57 @@
 import redis
 import mechanicalsoup
+import configparser 
+from elasticsearch import Elasticsearch, helpers
 
+config = configparser.ConfigParser()
+config.read('elastic.ini')
+print(config.read('elastic.ini'))
 
-url = "https://www.scrapingbee.com/webscraping-questions/beautifulsoup/how-to-find-all-links-using-beautifulsoup-and-python/"
-r = redis.Redis()
-def recursive_scrape(url):
+es = Elasticsearch(
+    cloud_id= config['ELASTIC']['cloud_id'],
+    http_auth=(config['ELASTIC']['user'], config['ELASTIC']['password'])
+)
+
+def write_to_elastic(es, url, html):
+    link = url.decode('utf-8')
+    es.index(
+        index= 'webpages', 
+        document={
+            'url': link,
+            'html': html
+        }
+    )
+
+# result = es.search(
+#     index='webpages',
+#     query={
+#         'match': {'html': 'html'}
+#     }
+# )
+
+def crawl(browser, r, es, url):
     print(url)
-    browser = mechanicalsoup.StatefulBrowser()
     browser.open(url)
+
+    #cache to elastic 
+    write_to_elastic(es, url, str(browser.page))
+
+    #get links
     links = browser.page.find_all("a")
-    texts = browser.page.find_all(text=True)
-    r.lpush('PageText', str(texts))
+    hrefs = [a.get("href") for a in links]
 
-    for link in links:
-        if "http" in link.get("href"):
-            r.lpush('NikeLinks', link.get("href"))
+    # Do filtering
+    domain = "https://en.wikipedia.org"
+    links = [domain + a for a in hrefs if a and a.startswith("/wiki/")]
 
-    while(r.llen('NikeLinks')!=0):
-        try:
-            next_link = r.rpop("NikeLinks")
-            if next_link != None and "http" in str(next_link):
-                recursive_scrape(next_link)
-        except:
-            print("Error occured")
+    print("pushing links to redis")
+    r.lpush("links", *links)
 
+start_url = "https://en.wikipedia.org/wiki/Redis"
+r = redis.Redis()
+browser = mechanicalsoup.StatefulBrowser()
 
-recursive_scrape(url)
+r.lpush("links", start_url)
+
+while link := r.rpop("links"):
+    crawl(browser, r, es, link)
